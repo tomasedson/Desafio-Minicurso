c4b00f77b1b36ce4e4975ae5a2a46d1e422cc3af
Tomas Edson <tomascipimoi@gmail.com>
2022-01-15 16:01:16 -0300

    alterando PegarPersongem para usar uma estrutura de repeticao

--- javascript/app.js
+++ javascript/app.js
@@ -8,32 +8,17 @@ pegarPersonagem = () => {
 
     PegarPersonagem().then((data) => {
 
-        document.querySelector('img#imagem-0').src = data[0].image;
-        document.querySelector('img#imagem-1').src = data[1].image;
-        document.querySelector('img#imagem-2').src = data[2].image;
-
         let mensagemAlt = "Imagem aleatória do personagem ";
 
-        document.querySelector('img#imagem-0').alt = `${mensagemAlt} ${data[0].name}`;
-        document.querySelector('img#imagem-1').alt =`${mensagemAlt} ${data[1].name}`;
-        document.querySelector('img#imagem-2').alt = `${mensagemAlt} ${data[2].name}`;
-
-        document.querySelector('p#nome-0').innerHTML = data[0].name;
-        document.querySelector('p#nome-1').innerHTML = data[1].name;
-        document.querySelector('p#nome-2').innerHTML = data[2].name;
-
-        document.querySelector('p#especie-0').innerHTML = data[0].species;
-        document.querySelector('p#especie-1').innerHTML = data[1].species;
-        document.querySelector('p#especie-2').innerHTML = data[2].species;
-
-        document.querySelector('p#condicao-0').innerHTML = data[0].status;
-        document.querySelector('p#condicao-1').innerHTML = data[1].status;
-        document.querySelector('p#condicao-2').innerHTML = data[2].status;
-        // imagem.alt = data.name;
-        // nomeDoPersonagem.innerHTML = data.name;
-        // espécie.innerHTML = data.species;
-        // condição.innerHTML = data.status;
-        console.log(data);
+        for (let index = 0; index < data.length; index++) {
+            
+            document.querySelector('img#imagem-' + index).src = data[index].image;
+            document.querySelector('img#imagem-' + index).alt = `${mensagemAlt} ${data[index].name}`;
+            document.querySelector('p#nome-' + index).innerHTML = data[index].name;
+            document.querySelector('p#especie-' + index).innerHTML = data[index].species;
+            document.querySelector('p#condicao-'+index).innerHTML = data[index].status;
+            
+        }
     });
 }
 
