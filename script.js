09d86cf04d88afb639debfc6edbbc64b2298c79a
Tomas Edson <tomascipimoi@gmail.com>
2022-01-15 13:03:37 -0300

    Capiturar elementos html manipulaveis

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img');
+const button = document.querySelector('button');
+const nomeDoPersonagem = document.querySelector('#Nome');
+const Espécie = document.querySelector('Espécie');
+const Condição = document.querySelector('Condição');
