ddceba0e1c98ef7e7ede2f19fc6a9d51ef9b44a9
Tomas Edson <tomascipimoi@gmail.com>
2022-01-15 13:59:45 -0300

    corrigir html

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const imagem = document.querySelector('img');
 const button = document.querySelector('button');
-const nomeDoPersonagem = document.querySelector('#Nome');
-const Espécie = document.querySelector('Espécie');
-const Condição = document.querySelector('Condição');
+const nomeDoPersonagem = document.querySelector('#nome');
+const Espécie = document.querySelector('espécie');
+const Condição = document.querySelector('condição');
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`,  {
+        method:'GET' ,
+        Headers: {
+           Accept: 'application/json',
+           "Content-type": 'application/json'
+        }
+    }).then((Response) => Response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt= data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        espécie.innerHTML = data.species;
+        condição.innerHTML = data.status;
+    });
+}
+
+button.onclick = pegarPersonagem;
