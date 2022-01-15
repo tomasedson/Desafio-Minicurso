9d444a5a0fcf5e8a93e4051aae0fa631328d5196
Tomas Edson <tomascipimoi@gmail.com>
2022-01-15 15:50:15 -0300

    Adicionando os valores do Json aos três itens aletórios na tela

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,4 @@
-const imagem = document.querySelector('img');
-const button = document.querySelector('button');
+const button = document.querySelector('button#visualizar-personagens');
 const nomeDoPersonagem = document.querySelector('#nome');
 const Espécie = document.querySelector('espécie');
 const Condição = document.querySelector('condição');
@@ -8,12 +7,33 @@ const Condição = document.querySelector('condição');
 pegarPersonagem = () => {
 
     PegarPersonagem().then((data) => {
-        // imagem.src = data.image;
+
+        document.querySelector('img#imagem-0').src = data[0].image;
+        document.querySelector('img#imagem-1').src = data[1].image;
+        document.querySelector('img#imagem-2').src = data[2].image;
+
+        let mensagemAlt = "Imagem aleatória do personagem ";
+
+        document.querySelector('img#imagem-0').alt = `${mensagemAlt} ${data[0].name}`;
+        document.querySelector('img#imagem-1').alt =`${mensagemAlt} ${data[1].name}`;
+        document.querySelector('img#imagem-2').alt = `${mensagemAlt} ${data[2].name}`;
+
+        document.querySelector('p#nome-0').innerHTML = data[0].name;
+        document.querySelector('p#nome-1').innerHTML = data[1].name;
+        document.querySelector('p#nome-2').innerHTML = data[2].name;
+
+        document.querySelector('p#especie-0').innerHTML = data[0].species;
+        document.querySelector('p#especie-1').innerHTML = data[1].species;
+        document.querySelector('p#especie-2').innerHTML = data[2].species;
+
+        document.querySelector('p#condicao-0').innerHTML = data[0].status;
+        document.querySelector('p#condicao-1').innerHTML = data[1].status;
+        document.querySelector('p#condicao-2').innerHTML = data[2].status;
         // imagem.alt = data.name;
         // nomeDoPersonagem.innerHTML = data.name;
         // espécie.innerHTML = data.species;
         // condição.innerHTML = data.status;
-        console.table(data);
+        console.log(data);
     });
 }
 
